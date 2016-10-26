e5569bedc9d2d5b471e36ecea6c054396815e82b
Brian Tom <btom.0831@gmail.com>
2016-10-25 21:18:36 -0700

    Updated with comments and link to animation

--- TLRgame/Assets/Assets/Scripts/PlayerAttack.cs
+++ TLRgame/Assets/Assets/Scripts/PlayerAttack.cs
@@ -5,28 +5,28 @@ public class PlayerAttack : MonoBehaviour {
 
     private bool isAttacking = false;
 
-    private float attackTimer = 0;
-    private float attackCooldown = 0.3f;
+    private float attackTimer = 0;       // keep track of when next attack can occur
+    private float attackCooldown = 0.3f; // minimum time between attacks
 
-    public Collider2D attackTrigger;
-    private Animator anim;
+    public Collider2D attackTrigger;     // 2D collision box (in front of player) for attacking
+    private Animator anim;               // attack animation
 
     void Awake()
     {
         anim = gameObject.GetComponent<Animator>();
-        attackTrigger.enabled = false;
+        attackTrigger.enabled = false; // turn off trigger collider
     }
 
     void Update()
     {
+        // Attack when space is pressed
         if (Input.GetKeyDown("space") && !isAttacking)
         {
-            print("Attack");
             isAttacking = true;
             attackTimer = attackCooldown;
             attackTrigger.enabled = true;
         }
-
+        // If the player is attacking, start cooldown timer and wait until attackTimer reaches 0 before next attack
         if (isAttacking)
         {
             if (attackTimer > 0)
@@ -39,5 +39,7 @@ public class PlayerAttack : MonoBehaviour {
                 attackTrigger.enabled = false;
             }
         }
+        // Change isAttacking bool in Unity editor for animation based on isAttacking bool in this script
+        anim.SetBool("isAttacking", isAttacking);
     }
 }
