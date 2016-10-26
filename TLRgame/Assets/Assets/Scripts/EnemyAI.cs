360d4c6e39404614a8687d1e94aed2f015a275b6
Brian Tom <btom.0831@gmail.com>
2016-10-26 01:11:53 -0700

    Add enemy AI script

---
+++ TLRgame/Assets/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+    int currHealth = 50;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (currHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+    // Called when player hits enemy, subtracts health from enemy and plays damaged animation
+    void Damage(int damage)
+    {
+        currHealth -= damage;
+        // TODO: red flash animation
+        gameObject.GetComponent<Animation>().Play("DamageRedFlash");
+    }
+}
