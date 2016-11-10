8574d7196c84f166cfea506e226a3540e6b1f08a
nina-y <nye2@uci.edu>
2016-11-09 21:00:56 -0800

    Transition
    Transition WIP

---
+++ TLRgame/Assets/Assets/Scripts/NewLevel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewLevel : MonoBehaviour {
+
+	public LevelHandler levelhandler;
+
+	void Awake() {
+		StartCoroutine(levelhandler.FadeToClear());
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
