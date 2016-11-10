375baef2a73e72ff3b6de4a4d3aec94bfcf6f2d6
Brian Tom <btom.0831@gmail.com>
2016-11-09 21:13:41 -0800

    Update physics

--- TLRgame/Assets/Assets/Scripts/Player.cs
+++ TLRgame/Assets/Assets/Scripts/Player.cs
@@ -8,75 +8,35 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    public Stat health;
-    public Stat exp;
-    public int lastFacing = 1;
+    [HideInInspector] public bool facingRight = true;
 	public float speed = 50f;
+    public float moveForce = 365f;
 	public float maxSpeed = 5f;
-	public float jumpPower = 150f;
-	public float friction = 0.75f;
+	public float jumpForce = 150f;
+    public float friction = 0.8f;
 	public bool isGrounded;
 	public bool isDoubleOK;
 	public bool isDead = false;
+    public Stat health;
+    public Stat exp;
 
-	// References
-	private Rigidbody2D rb2D;
+    private Rigidbody2D rb2D;
 	private Animator anim;
 
     public void Awake()
     {
         health.Initialize();
         exp.Initialize();
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
     }
-
-	// Use this for initialization
-	void Start ()
-	{
-		rb2D = gameObject.GetComponent<Rigidbody2D>();
-		anim = gameObject.GetComponent<Animator>();
-	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-		anim.SetBool("isGrounded", isGrounded);             // associating isGrounded from Unity to isGrounded in script
-		anim.SetFloat("Speed", Mathf.Abs(rb2D.velocity.x)); // only a positive value
-		anim.SetBool("isDead", isDead);                     // dead when currHealth <= 0
-        // Player faces last facing direction when idle
-        if (Input.GetAxis("Horizontal") == 0)
-        {
-            transform.localScale = new Vector3(lastFacing, 1, 1);
-        }
-		// Walking to the left
-		if (Input.GetAxis("Horizontal") < 0)
-        {
-			transform.localScale = new Vector3 (-1, 1, 1);
-            lastFacing = -1;
-		}
-		// Walking to the right
-		if (Input.GetAxis("Horizontal") > 0)
-        {
-			transform.localScale = new Vector3 (1, 1, 1);
-            lastFacing = 1;
-		}
-        // Jumping
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (isGrounded)
-            {
-                rb2D.AddForce(Vector2.up * jumpPower);
-                isDoubleOK = true;
-            }
-            else
-            {
-                if (isDoubleOK)
-                {
-                    rb2D.velocity = new Vector2(rb2D.velocity.x, 0);
-                    rb2D.AddForce(Vector2.up * jumpPower);
-                    isDoubleOK = false;
-                }
-            }
-        }
+		anim.SetBool("isGrounded", isGrounded);
+		anim.SetBool("isDead", isDead);
+
         // Reduce health test
         if (Input.GetKeyDown(KeyCode.Q))
         {
@@ -102,29 +62,63 @@ public class Player : MonoBehaviour
 	// FixedUpdated called for physics stuff
 	void FixedUpdate ()
 	{
-		// Fake friction to ease velocity
-		Vector3 easeVelocity = rb2D.velocity;
-		if (Input.GetAxis("Horizontal") < 1f && Input.GetAxis("Horizontal") > -1f) { // prevent friction from activating when moving
-			easeVelocity.x *= friction; // decrease magnitude of x
-		}
-		easeVelocity.y = rb2D.velocity.y; // friction does not affect jump
-		easeVelocity.z = 0.0f; // no z-axis change
-		if (isGrounded) {
-			rb2D.velocity = easeVelocity;
-		}
-		// horizontal movement
-		float x = Input.GetAxis("Horizontal"); // -1 or +1 for left and right
-		rb2D.AddForce (Vector2.right * speed * x); // multiply 2D vector by magnitude (speed) and direction (x)
-		// limiting horizontal movement
-		if (rb2D.velocity.x > maxSpeed)
+		// Horizontal movement
+		float x = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(x));
+
+        rb2D.AddForce(Vector2.right * x * moveForce);
+
+        // Fake friction to ease velocity
+        Vector2 easeVelocity = rb2D.velocity;
+        if (x < 1f && x > -1f)
         {
-			rb2D.velocity = new Vector2 (maxSpeed, rb2D.velocity.y);
-		}
-		if (rb2D.velocity.x < -maxSpeed)
+            easeVelocity.x *= friction; // decrease magnitude of x
+        }
+        easeVelocity.y = rb2D.velocity.y; // friction does not affect jump
+        if (isGrounded)
+            rb2D.velocity = easeVelocity;
+        
+        // Limit speed
+        if (rb2D.velocity.x > maxSpeed)
+            rb2D.velocity = new Vector2(maxSpeed, rb2D.velocity.y);
+        if (rb2D.velocity.x < -maxSpeed)
+            rb2D.velocity = new Vector2(-maxSpeed, rb2D.velocity.y);
+
+        // Flip direction of player
+        if (x < 0 && facingRight)
+            Flip();
+        if (x > 0 && !facingRight)
+            Flip();
+
+        // Jumping and double jumping
+        if (Input.GetButtonDown("Jump"))
         {
-			rb2D.velocity = new Vector2(-maxSpeed, rb2D.velocity.y);
-		}
-	}
+            if (isGrounded)
+            {
+                rb2D.AddForce(Vector2.up * jumpForce);
+                isDoubleOK = true;
+            }
+            else
+            {
+                if (isDoubleOK)
+                {
+                    rb2D.velocity = new Vector2(rb2D.velocity.x, 0);
+                    rb2D.AddForce(Vector2.up * jumpForce);
+                    isDoubleOK = false;
+                }
+            }
+        }
+    }
+
+    // Changes direction player is facing
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
 
     public void Damage(int dmg)
     {
