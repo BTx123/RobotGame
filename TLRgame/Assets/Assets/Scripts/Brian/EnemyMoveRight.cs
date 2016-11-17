60f6ff9f171b81725c15d99b59817038f0f93ec3
Brian Tom <btom.0831@gmail.com>
2016-11-16 22:36:41 -0800

    Add move right script for testing

---
+++ TLRgame/Assets/Assets/Scripts/Brian/EnemyMoveRight.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMoveRight : MonoBehaviour {
+
+    Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Awake () {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        rb2D.velocity = new Vector2(10, rb2D.velocity.y);
+    }
+}
